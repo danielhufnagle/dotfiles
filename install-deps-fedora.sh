0bfadcf4b88e33abfc5176cc3dd0b3a15bb586a4
danielhufnagle <danielchufnagle@gmail.com>
2024-03-06 16:03:14 -0500

    added dependency installer for fedora/nobara

---
+++ install-deps-fedora.sh
@@ -0,0 +1,3 @@
+dnf install neovim zsh util-linux wofi waybar swaybg network-manager-applet
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
