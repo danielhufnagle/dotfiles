3ed5d3c4482e012238e6b7c50332199358b7305b
danielhufnagle <danielchufnagle@gmail.com>
2024-03-06 15:27:26 -0500

    added everything

---
+++ update-dots.sh
@@ -0,0 +1,5 @@
+cp -r ~/.config/hypr ~/dotfiles-hyprland
+cp -r ~/.config/kitty ~/dotfiles-hyprland
+cp -r ~/.config/waybar ~/dotfiles-hyprland
+cp -r ~/.config/wofi ~/dotfiles-hyprland
+cp -r ~/.config/neofetch ~/dotfiles-hyprland
